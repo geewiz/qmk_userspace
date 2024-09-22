4581037c58f45dc8cb69bdf031bf679472fd2850
Jochen Lillich <contact@geewiz.dev>
2024-09-22 15:28:35 +0100

    doio/kb38:geewiz: Improve ETS2 keymap

--- keyboards/doio/kb38/keymaps/geewiz/keymap.c
+++ keyboards/doio/kb38/keymaps/geewiz/keymap.c
@@ -75,12 +75,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LAYERTHREE] = LAYOUT(
         // Euro Truck Simulator
-        TO(3),              ET_STEA ,              KC_L    , KC_K    , KC_O    ,    KC_P    , KC_NO   , KC_NO  ,
-        KC_E    , KC_SPC  , KC_NO   , KC_T    ,    KC_F7   , KC_F8   , KC_F9   ,
-        KC_7    , KC_8    , KC_9    , KC_I    ,    KC_F4   , KC_F5   , KC_F6   ,    KC_C    ,           KC_R   ,
-        KC_4    , KC_5    , KC_6    ,              KC_F1   , KC_F2   , KC_F3   ,
-        KC_1    , KC_2    , KC_3    , KC_ENT  ,              KC_UP   ,                        KC_NO   ,
-        KC_ESC  ,           KC_NO   ,              KC_LEFT , KC_DOWN , KC_RGHT
+        TO(3),              KC_ESC  ,              KC_F10  , KC_F11  , KC_F12  ,    KC_M   , KC_F14 , KC_F15 ,
+        KC_NUM  , KC_PSLS , KC_PAST , KC_PMNS ,    KC_F7   , KC_F8   , KC_F9   ,
+        KC_P7   , KC_P8   , KC_P9   , KC_PPLS ,    KC_F4   , KC_F5   , KC_F6   ,    ET_STEA,          KC_MUTE,
+        KC_P4   , KC_P5   , KC_P6   ,              KC_F1   , KC_F2   , KC_F3   ,
+        KC_P1   , KC_P2   , KC_P3   , KC_PENT ,              KC_UP   ,                       KC_NO  ,
+        KC_P0   ,           KC_PDOT ,              KC_LEFT , KC_DOWN , KC_RGHT
     ),
     [_LAYERFOUR] = LAYOUT(
         TO(0),              KC_NO   ,              RGB_RMOD, RGB_TOG , RGB_MOD ,    KC_NO   , QK_RBT  , QK_BOOT,
@@ -111,9 +111,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
         ENCODER_CCW_CW(KC_LEFT, KC_RGHT)
     },
     [_LAYERTHREE] = {
-        ENCODER_CCW_CW(KC_PGDN, KC_PGUP),
-        ENCODER_CCW_CW(KC_MS_WH_DOWN, KC_MS_WH_UP),
-        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
+        ENCODER_CCW_CW(KC_PGUP, KC_PGDN),
+        ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN)
     },
     [_LAYERFOUR] = {
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
