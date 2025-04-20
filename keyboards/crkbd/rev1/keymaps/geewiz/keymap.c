6dfab77bbc491de8b64a75262e787686360e1c7c
Jochen Lillich <contact@geewiz.dev>
2025-04-20 10:11:26 +0100

    fix(crkbd): Mod thumb key sides

--- keyboards/crkbd/rev1/keymaps/geewiz/keymap.c
+++ keyboards/crkbd/rev1/keymaps/geewiz/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       U_UMLOE, KC_Z   , KC_X   , KC_C   , KC_D   , KC_V   ,                      KC_K   , KC_H   , KC_COMM, KC_DOT , KC_SLSH, KC_MINS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, LY_NUMB, KC_LSFT,    KC_SPC , LY_SYMB, XXXXXXX \
+                                          KC_LGUI, LY_NUMB, OS_LSFT,    KC_SPC , LY_SYMB, KC_LALT \
                                       //`--------------------------'  `--------------------------'
   ),
 #else
